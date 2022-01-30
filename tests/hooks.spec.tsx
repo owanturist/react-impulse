ae33dfd8f68d7d0186760c3a5f0bb5538022a8a1
Anton Ovechkin <owanturist@gmail.com>
2022-01-30 12:53:24 +0100

    Add Watch multiple store

--- tests/hooks.spec.tsx
+++ tests/hooks.spec.tsx
@@ -580,7 +580,7 @@ describe("Watch single store", () => {
     act(() => {
       store.setState((state) => state + 1)
     })
-    expect(onRender).toHaveBeenCalledTimes(2)
+    expect(onRender).toHaveBeenCalledTimes(2) // does not re-render
     expect(onCountRender).toHaveBeenCalledTimes(4)
     expect(screen.queryByText("more than one")).toBeInTheDocument()
     expect(screen.queryByText("less than four")).toBeInTheDocument()
@@ -593,5 +593,270 @@ describe("Watch single store", () => {
     expect(screen.queryByText("more than one")).toBeInTheDocument()
     expect(screen.queryByText("less than four")).not.toBeInTheDocument()
     expect(screen.getByTestId("count")).toHaveTextContent("4")
+  })
+})
+
+describe("Watch multiple store", () => {
+  interface AppProps {
+    firstCount: InnerStore<number>
+    secondCount: InnerStore<number>
+    onRender: VoidFunction
+    onFirstCountRender: VoidFunction
+    onSecondCountRender: VoidFunction
+  }
+
+  const SingleWatcherApp: React.VFC<AppProps> = ({
+    firstCount,
+    secondCount,
+    onFirstCountRender,
+    onSecondCountRender,
+    onRender,
+  }) => {
+    const [moreThenOne, lessThanFour] = useInnerWatch(
+      () => {
+        const sum = firstCount.getState() + secondCount.getState()
+
+        return [sum > 2, sum < 7]
+      },
+      ([left1, right1], [left2, right2]) => {
+        return left1 === left2 && right1 === right2
+      },
+    )
+    onRender()
+
+    return (
+      <>
+        {moreThenOne && <span>more than two</span>}
+        {lessThanFour && <span>less than seven</span>}
+
+        <button
+          type="button"
+          data-testid="increment-both"
+          onClick={() => {
+            firstCount.setState((state) => state + 1)
+            secondCount.setState((state) => state + 1)
+          }}
+        />
+
+        <CounterComponent count={firstCount} onRender={onFirstCountRender} />
+        <CounterComponent count={secondCount} onRender={onSecondCountRender} />
+      </>
+    )
+  }
+
+  const SingleMemoizedWatcherApp: React.VFC<AppProps> = ({
+    firstCount,
+    secondCount,
+    onFirstCountRender,
+    onSecondCountRender,
+    onRender,
+  }) => {
+    const [moreThenOne, lessThanFour] = useInnerWatch(
+      React.useCallback(() => {
+        const sum = firstCount.getState() + secondCount.getState()
+
+        return [sum > 2, sum < 7]
+      }, [firstCount, secondCount]),
+      React.useCallback(([left1, right1], [left2, right2]) => {
+        return left1 === left2 && right1 === right2
+      }, []),
+    )
+    onRender()
+
+    return (
+      <>
+        {moreThenOne && <span>more than two</span>}
+        {lessThanFour && <span>less than seven</span>}
+
+        <button
+          type="button"
+          data-testid="increment-both"
+          onClick={() => {
+            firstCount.setState((state) => state + 1)
+            secondCount.setState((state) => state + 1)
+          }}
+        />
+
+        <CounterComponent count={firstCount} onRender={onFirstCountRender} />
+        <CounterComponent count={secondCount} onRender={onSecondCountRender} />
+      </>
+    )
+  }
+
+  const MultipleWatchersApp: React.VFC<AppProps> = ({
+    firstCount,
+    secondCount,
+    onFirstCountRender,
+    onSecondCountRender,
+    onRender,
+  }) => {
+    const moreThenOne = useInnerWatch(() => {
+      const sum = firstCount.getState() + secondCount.getState()
+
+      return sum > 2
+    })
+    const lessThanFour = useInnerWatch(() => {
+      const sum = firstCount.getState() + secondCount.getState()
+
+      return sum < 7
+    })
+    onRender()
+
+    return (
+      <>
+        {moreThenOne && <span>more than two</span>}
+        {lessThanFour && <span>less than seven</span>}
+
+        <button
+          type="button"
+          data-testid="increment-both"
+          onClick={() => {
+            firstCount.setState((state) => state + 1)
+            secondCount.setState((state) => state + 1)
+          }}
+        />
+
+        <CounterComponent count={firstCount} onRender={onFirstCountRender} />
+        <CounterComponent count={secondCount} onRender={onSecondCountRender} />
+      </>
+    )
+  }
+
+  const MultipleMemoizedWatchersApp: React.VFC<AppProps> = ({
+    firstCount,
+    secondCount,
+    onFirstCountRender,
+    onSecondCountRender,
+    onRender,
+  }) => {
+    const moreThenOne = useInnerWatch(
+      React.useCallback(() => {
+        const sum = firstCount.getState() + secondCount.getState()
+
+        return sum > 2
+      }, [firstCount, secondCount]),
+    )
+    const lessThanFour = useInnerWatch(
+      React.useCallback(() => {
+        const sum = firstCount.getState() + secondCount.getState()
+
+        return sum < 7
+      }, [firstCount, secondCount]),
+    )
+    onRender()
+
+    return (
+      <>
+        {moreThenOne && <span>more than two</span>}
+        {lessThanFour && <span>less than seven</span>}
+
+        <button
+          type="button"
+          data-testid="increment-both"
+          onClick={() => {
+            firstCount.setState((state) => state + 1)
+            secondCount.setState((state) => state + 1)
+          }}
+        />
+
+        <CounterComponent count={firstCount} onRender={onFirstCountRender} />
+        <CounterComponent count={secondCount} onRender={onSecondCountRender} />
+      </>
+    )
+  }
+
+  it.each([
+    ["single watcher", SingleWatcherApp],
+    ["single memoized watcher", SingleMemoizedWatcherApp],
+    ["multiple watchers", MultipleWatchersApp],
+    ["multiple memoized watchers", MultipleMemoizedWatchersApp],
+  ])("watches multiple stores with %s", (_, App) => {
+    const firstCount = InnerStore.of(0)
+    const secondCount = InnerStore.of(0)
+    const onFirstCountRender = jest.fn()
+    const onSecondCountRender = jest.fn()
+    const onRender = jest.fn()
+
+    render(
+      <App
+        firstCount={firstCount}
+        secondCount={secondCount}
+        onFirstCountRender={onFirstCountRender}
+        onSecondCountRender={onSecondCountRender}
+        onRender={onRender}
+      />,
+    )
+
+    // initial render and watcher setup
+    expect(onRender).toHaveBeenCalledTimes(1)
+    expect(onFirstCountRender).toHaveBeenCalledTimes(1)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(1)
+    expect(screen.queryByText("more than two")).not.toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("0")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("0")
+
+    // increment first count
+    fireEvent.click(withinNth("counter", 0).getByTestId("increment"))
+    expect(onRender).toHaveBeenCalledTimes(1) // does not re-render
+    expect(onFirstCountRender).toHaveBeenCalledTimes(2)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(1)
+    expect(screen.queryByText("more than two")).not.toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("1")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("0")
+
+    // increment second count
+    fireEvent.click(withinNth("counter", 1).getByTestId("increment"))
+    expect(onRender).toHaveBeenCalledTimes(1) // does not re-render
+    expect(onFirstCountRender).toHaveBeenCalledTimes(2)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(2)
+    expect(screen.queryByText("more than two")).not.toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("1")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("1")
+
+    // increment both
+    fireEvent.click(screen.getByTestId("increment-both"))
+    expect(onRender).toHaveBeenCalledTimes(2)
+    expect(onFirstCountRender).toHaveBeenCalledTimes(3)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(3)
+    expect(screen.queryByText("more than two")).toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("2")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("2")
+
+    // increment both again
+    fireEvent.click(screen.getByTestId("increment-both"))
+    expect(onRender).toHaveBeenCalledTimes(2) // does not re-render
+    expect(onFirstCountRender).toHaveBeenCalledTimes(4)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(4)
+    expect(screen.queryByText("more than two")).toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("3")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("3")
+
+    // increment first
+    fireEvent.click(withinNth("counter", 0).getByTestId("increment"))
+    expect(onRender).toHaveBeenCalledTimes(3)
+    expect(onFirstCountRender).toHaveBeenCalledTimes(5)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(4)
+    expect(screen.queryByText("more than two")).toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).not.toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("4")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("3")
+
+    // increment both from the outside
+    act(() => {
+      firstCount.setState((state) => state + 1)
+      secondCount.setState((state) => state + 1)
+    })
+    expect(onRender).toHaveBeenCalledTimes(3) // does not re-render
+    expect(onFirstCountRender).toHaveBeenCalledTimes(6)
+    expect(onSecondCountRender).toHaveBeenCalledTimes(5)
+    expect(screen.queryByText("more than two")).toBeInTheDocument()
+    expect(screen.queryByText("less than seven")).not.toBeInTheDocument()
+    expect(screen.getAllByTestId("count")[0]).toHaveTextContent("5")
+    expect(screen.getAllByTestId("count")[1]).toHaveTextContent("4")
   })
 })
