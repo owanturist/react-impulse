31635069053c1124778ffaa2493711b94834f3f1
Anton Ovechkin <owanturist@gmail.com>
2022-05-21 19:26:22 +0200

    Use testing-library/react but not react-hooks

--- tests/bugfixes.spec.tsx
+++ tests/bugfixes.spec.tsx
@@ -1,4 +1,4 @@
-import { act, fireEvent, render, screen } from "@testing-library/react-hooks"
+import { act, fireEvent, render, screen } from "@testing-library/react"
 import React from "react"
 
 import { Sweety, useSweetyState, useWatchSweety } from "../src"
