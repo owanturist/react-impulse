ba37083a63ce8fb3b28911e2e30edd9bbbac3805
Anton Ovechkin <owanturist@gmail.com>
2022-08-18 00:31:03 +0200

    Delete IS_REACT_ACT_ENVIRONMENT from setupTests

--- setupTests.ts
+++ setupTests.ts
@@ -1,6 +1,3 @@
 // otherwise jest leaking into vitest type definitions
 // https://github.com/testing-library/jest-dom/issues/427#issuecomment-1110985202
 import "@testing-library/jest-dom/extend-expect"
-
-// @ts-expect-error https://reactjs.org/blog/2022/03/08/react-18-upgrade-guide.html#configuring-your-testing-environment
-globalThis.IS_REACT_ACT_ENVIRONMENT = true
