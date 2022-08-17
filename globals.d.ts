296f52c1736586bde5a243e56490aa8d81153ee4
Anton Ovechkin <owanturist@gmail.com>
2022-08-17 22:07:45 +0200

    Stop jest types leaking to vite

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,12 @@
+import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
+
+declare global {
+  namespace jest {
+    interface Matchers<R = void>
+      extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+  }
+}
+
 interface Process {
   env: ProcessEnv
 }
