f117e02a7bd2468e302c5efc383f95bcf61f9f73
Anton Ovechkin <owanturist@gmail.com>
2022-10-15 22:00:22 +0200

    Add spec to check default stores

---
+++ tests/hooks-in-components/default-stores.spec.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import { act, render, screen, fireEvent } from "@testing-library/react"
+
+import { Sweety, useSweety, useSweetyState } from "../../src"
+
+describe("default stores", () => {
+  it("Uses local default Sweety when nullable", () => {
+    const SearchBar: React.FC<{
+      value?: Sweety<string>
+    }> = ({ value: valueStore }) => {
+      const defaultValueStore = useSweety("search for me")
+
+      const [value, setValue] = useSweetyState(valueStore ?? defaultValueStore)
+
+      return (
+        <input
+          role="search"
+          value={value}
+          onChange={(event) => setValue(event.target.value)}
+        />
+      )
+    }
+
+    const { rerender, unmount } = render(<SearchBar />)
+
+    const input = screen.getByRole("search")
+
+    expect(input).toHaveValue("search for me")
+
+    fireEvent.change(input, { target: { value: "now" } })
+    expect(input).toHaveValue("now")
+
+    const val = Sweety.of("new")
+
+    rerender(<SearchBar value={val} />)
+    expect(input).toHaveValue("new")
+
+    fireEvent.change(input, { target: { value: "what a hell" } })
+    expect(input).toHaveValue("what a hell")
+
+    act(() => {
+      val.setState("no way")
+    })
+    expect(input).toHaveValue("no way")
+
+    rerender(<SearchBar />)
+    expect(input).toHaveValue("now")
+
+    act(() => {
+      val.setState("but now")
+    })
+    expect(input).toHaveValue("now")
+
+    unmount()
+    render(<SearchBar value={val} />)
+    expect(screen.getByRole("search")).toHaveValue("but now")
+  })
+})
