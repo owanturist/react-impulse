bce3cd31a1d8ed3202d5ac762a9dab4b6633c715
Anton Ovechkin <owanturist@gmail.com>
2022-10-29 23:04:32 +0200

    Split useSweetyMemo

--- src/watch.ts
+++ src/watch.ts
@@ -1,20 +1,7 @@
-import { FC, useMemo } from "react"
-import { useSyncExternalStore } from "use-sync-external-store/shim"
+import { FC } from "react"
 import { useSyncExternalStoreWithSelector } from "use-sync-external-store/shim/with-selector.js"
 
 import { useWatchContext } from "./useWatchContext"
-
-export const useSweetyMemo: typeof useMemo = (factory, deps) => {
-  const { context, subscribe, getState } = useWatchContext()
-
-  const buster = useSyncExternalStore(subscribe, getState, getState)
-
-  return useMemo(
-    () => context.watchStores(factory),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    deps && [...deps, buster],
-  )
-}
 
 export function watch<TProps extends object>(fc: FC<TProps>): FC<TProps> {
   const SweetyWatcher: FC<TProps> = (props, ctx) => {
