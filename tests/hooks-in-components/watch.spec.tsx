c33b7de8c781fdedca6e6b50b6496c605a5dc4d7
Anton Ovechkin <owanturist@gmail.com>
2022-11-21 16:33:26 +0100

    Test with testing-library/react but not react-hooks

--- tests/hooks-in-components/watch.spec.tsx
+++ tests/hooks-in-components/watch.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, act } from "@testing-library/react-hooks"
+import { render, screen, fireEvent, act } from "@testing-library/react"
 import React from "react"
 
 import { Sweety, watch } from "../../src"
