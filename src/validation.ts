8f9f3c94359fb5ef086ae3d4025a96e50b889d35
Anton Ovechkin <owanturist@gmail.com>
2023-03-22 11:49:48 +0100

    Add WarningSource "subscribe"

--- src/validation.ts
+++ src/validation.ts
@@ -1,9 +1,14 @@
-export type WarningSource = "useWatchImpulse" | "useImpulseMemo" | "watch"
+export type WarningSource =
+  | "useWatchImpulse"
+  | "useImpulseMemo"
+  | "watch"
+  | "subscribe"
 
 export type WarningSet = Record<WarningSource, null | string>
 
-interface MakeHookWarningOptions {
+interface MakeWarningOptions {
   isWatchAffected: boolean
+  isSubscribeAffected: boolean
   isCritical: boolean
   whatItDoes: string
   method: string
@@ -11,19 +16,47 @@ interface MakeHookWarningOptions {
 
 const makeHookWarningMessage = (
   hook: string,
-  { isCritical, whatItDoes, method }: MakeHookWarningOptions,
+  { isCritical, whatItDoes, method }: MakeWarningOptions,
 ): string => {
   const verb = isCritical ? "may" : "should"
 
   return `You ${verb} not call ${method} inside of the ${hook} callback. The ${hook} hook is for read-only operations but ${method} ${whatItDoes}.`
 }
 
-const makeWarningSet = (options: MakeHookWarningOptions): WarningSet => ({
+const makeWatchWarningMessage = ({
+  isWatchAffected,
+  isCritical,
+  method,
+}: MakeWarningOptions): null | string => {
+  if (!isWatchAffected) {
+    return null
+  }
+
+  const verb = isCritical ? "may" : "should"
+
+  return `You ${verb} not call ${method} during rendering of watch(Component)`
+}
+
+const makeSubscribeWarningMessage = ({
+  isSubscribeAffected,
+  isCritical,
+  method,
+  whatItDoes,
+}: MakeWarningOptions): null | string => {
+  if (!isSubscribeAffected) {
+    return null
+  }
+
+  const verb = isCritical ? "may" : "should"
+
+  return `You ${verb} not call ${method} inside of the subscribe listener. The listener is for read-only operations but ${method} ${whatItDoes}.`
+}
+
+const makeWarningSet = (options: MakeWarningOptions): WarningSet => ({
   useWatchImpulse: makeHookWarningMessage("useWatchImpulse(watcher)", options),
   useImpulseMemo: makeHookWarningMessage("useImpulseMemo(factory)", options),
-  watch: options.isWatchAffected
-    ? `You may not call ${options.method} during rendering of watch(Component)`
-    : null,
+  watch: makeWatchWarningMessage(options),
+  subscribe: makeSubscribeWarningMessage(options),
 })
 
 export const WARNING_MESSAGE_CALLING_OF_WHEN_WATCHING = makeWarningSet({
@@ -31,6 +64,7 @@ export const WARNING_MESSAGE_CALLING_OF_WHEN_WATCHING = makeWarningSet({
   whatItDoes: "creates a new Impulse",
   isCritical: false,
   isWatchAffected: false,
+  isSubscribeAffected: true,
 })
 
 export const WARNING_MESSAGE_CALLING_CLONE_WHEN_WATCHING = makeWarningSet({
@@ -38,6 +72,7 @@ export const WARNING_MESSAGE_CALLING_CLONE_WHEN_WATCHING = makeWarningSet({
   whatItDoes: "clones an existing Impulse",
   isCritical: false,
   isWatchAffected: false,
+  isSubscribeAffected: true,
 })
 
 export const WARNING_MESSAGE_CALLING_SET_VALUE_WHEN_WATCHING = makeWarningSet({
@@ -45,6 +80,7 @@ export const WARNING_MESSAGE_CALLING_SET_VALUE_WHEN_WATCHING = makeWarningSet({
   whatItDoes: "changes an existing Impulse",
   isCritical: true,
   isWatchAffected: true,
+  isSubscribeAffected: false,
 })
 
 export const WARNING_MESSAGE_CALLING_SUBSCRIBE_WHEN_WATCHING = makeWarningSet({
@@ -52,4 +88,5 @@ export const WARNING_MESSAGE_CALLING_SUBSCRIBE_WHEN_WATCHING = makeWarningSet({
   whatItDoes: "subscribes to an Impulse",
   isCritical: true,
   isWatchAffected: true,
+  isSubscribeAffected: true,
 })
