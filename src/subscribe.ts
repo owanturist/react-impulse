c53d42f89d1588b6ee065fe67b3405564898c2c3
Anton Ovechkin <owanturist@gmail.com>
2023-03-25 11:18:00 +0100

    Add docs

--- src/subscribe.ts
+++ src/subscribe.ts
@@ -1,5 +1,11 @@
 import { WatchContext } from "./WatchContext"
 
+/**
+ * A function that subscribes to changes of all `Impulse` instances that call the `Impulse#getValue` method inside the `listener`.
+ *
+ * @param listener function that will be called on each `Impulse` change, involved in the `listener` execution. Calls first time synchronously when `subscribe` is called.
+ * @returns cleanup function that unsubscribes the `listener`
+ */
 export const subscribe = (listener: VoidFunction): VoidFunction => {
   const context = new WatchContext("subscribe")
 
