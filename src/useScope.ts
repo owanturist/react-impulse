79ea943726328af4221f279bd3bfb8f2f90bea6d
Anton Ovechkin <owanturist@gmail.com>
2023-03-26 22:32:03 +0200

    Add transform to useScope

--- src/useScope.ts
+++ src/useScope.ts
@@ -1,10 +1,14 @@
-import { useSyncExternalStore } from "use-sync-external-store/shim"
-import { useMemo, useState } from "react"
+import { useSyncExternalStoreWithSelector } from "use-sync-external-store/shim/with-selector.js"
+import { useCallback, useMemo, useState } from "react"
 
 import { WatchContext } from "./WatchContext"
 import { SCOPE_KEY, Scope } from "./Scope"
+import { Compare } from "./utils"
 
-export const useScope = (): Scope => {
+export function useScope<T = Scope>(
+  transform?: (scope: Scope) => T,
+  compare?: Compare<T>,
+): T {
   const [context] = useState(() => new WatchContext())
 
   const [subscribe, getVersion] = useMemo(
@@ -15,13 +19,20 @@ export const useScope = (): Scope => {
     [context],
   )
 
-  const version = useSyncExternalStore(subscribe, getVersion, getVersion)
+  const select = useCallback(
+    (version: number) => {
+      const scope: Scope = { [SCOPE_KEY]: context, version }
 
-  return useMemo(
-    () => ({
-      [SCOPE_KEY]: context,
-      version,
-    }),
-    [context, version],
+      return transform == null ? (scope as T) : transform(scope)
+    },
+    [context, transform],
+  )
+
+  return useSyncExternalStoreWithSelector(
+    subscribe,
+    getVersion,
+    getVersion,
+    select,
+    compare,
   )
 }
