c206017d5547d5e1e31cbd609cc0295a658aba55
Anton Ovechkin <owanturist@gmail.com>
2023-04-08 09:39:19 +0200

    Replace useSyncExternalStore with useScope in useImpulseValue

--- src/useImpulseValue.ts
+++ src/useImpulseValue.ts
@@ -1,8 +1,7 @@
-import { useDebugValue, useMemo } from "react"
-import { useSyncExternalStore } from "use-sync-external-store/shim/index.js"
+import { useDebugValue } from "react"
 
 import type { Impulse } from "./Impulse"
-import { STATIC_SCOPE } from "./Scope"
+import { useScope } from "./useScope"
 
 /**
  * A hook that subscribes to the `impulse` changes and returns the current value.
@@ -12,15 +11,8 @@ import { STATIC_SCOPE } from "./Scope"
  * @version 1.0.0
  */
 export function useImpulseValue<T>(impulse: Impulse<T>): T {
-  const [subscribe, getSnapshot] = useMemo(
-    () => [
-      (onChange: VoidFunction) => impulse.subscribe(onChange),
-      // does not need a real Scope - it subscribes by itself
-      () => impulse.getValue(STATIC_SCOPE),
-    ],
-    [impulse],
-  )
-  const value = useSyncExternalStore(subscribe, getSnapshot)
+  const scope = useScope()
+  const value = impulse.getValue(scope)
 
   useDebugValue(value)
 
