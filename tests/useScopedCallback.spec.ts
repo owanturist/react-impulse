83c327b0bcdf5a93e1e16895d852f92c4dbc8fd2
Anton Ovechkin <owanturist@gmail.com>
2023-04-25 22:22:39 +0200

    Extend useScopedCallback.spec

--- tests/useScopedCallback.spec.ts
+++ tests/useScopedCallback.spec.ts
@@ -37,23 +37,37 @@ describe("single Impulse", () => {
     expect(count).toHaveProperty("emitters.size", 0)
   })
 
-  describe("when Impulse value changes", () => {
-    it.concurrent(
-      "does not change resulting function when not attached Impulse's value changes",
-      () => {
-        const count = Impulse.of(1)
-        const { result } = setup(count)
-        const initial = result.current
+  it.concurrent(
+    "does not change resulting function when not attached Impulse's value changes",
+    () => {
+      const count = Impulse.of(1)
+      const { result } = setup(count)
+      const initial = result.current
 
-        act(() => {
-          count.setValue(2)
-        })
+      act(() => {
+        count.setValue(2)
+      })
 
-        expect(result.current).toBe(initial)
-      },
-    )
+      expect(result.current).toBe(initial)
+    },
+  )
 
-    it.concurrent("changes resulting function", () => {
+  it.concurrent("the resulting function returns a value", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    expect(result.current()).toBe(2)
+
+    act(() => {
+      count.setValue(2)
+    })
+
+    expect(result.current()).toBe(4)
+  })
+
+  it.concurrent(
+    "changes resulting function when attached Impulse's value changes",
+    () => {
       const count = Impulse.of(1)
       const { result } = setup(count)
       const initial = result.current
@@ -65,9 +79,12 @@ describe("single Impulse", () => {
       })
 
       expect(result.current).not.toBe(initial)
-    })
+    },
+  )
 
-    it.concurrent("de-attaches an Impulse", () => {
+  it.concurrent(
+    "de-attaches an attached Impulse when its value changes",
+    () => {
       const count = Impulse.of(1)
       const { result } = setup(count)
 
@@ -78,35 +95,47 @@ describe("single Impulse", () => {
       })
 
       expect(count).toHaveProperty("emitters.size", 0)
-    })
+    },
+  )
+
+  it.concurrent("attach an Impulse when the resulting function calls", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    result.current()
+
+    expect(count).toHaveProperty("emitters.size", 1)
   })
 
-  describe("when the resulting function calls", () => {
-    it.concurrent("attach an Impulse", () => {
+  it.concurrent(
+    "attaches an Impulse only once on the subsequent resulting function calls",
+    () => {
       const count = Impulse.of(1)
       const { result } = setup(count)
 
       result.current()
+      result.current()
 
       expect(count).toHaveProperty("emitters.size", 1)
-    })
-  })
+    },
+  )
 
-  describe("when re-renders", () => {
-    it.concurrent(
-      "does not change resulting function with same Impulse",
-      () => {
-        const count = Impulse.of(1)
-        const { result, rerender } = setup(count)
-        const initial = result.current
+  it.concurrent(
+    "does not change resulting function with same Impulse when re-renders",
+    () => {
+      const count = Impulse.of(1)
+      const { result, rerender } = setup(count)
+      const initial = result.current
 
-        rerender(count)
+      rerender(count)
 
-        expect(result.current).toBe(initial)
-      },
-    )
+      expect(result.current).toBe(initial)
+    },
+  )
 
-    it.concurrent("changes resulting function by a new Impulse", () => {
+  it.concurrent(
+    "changes resulting function by a new Impulse when re-renders",
+    () => {
       const count_1 = Impulse.of(1)
       const { result, rerender } = setup(count_1)
       const initial = result.current
@@ -117,17 +146,109 @@ describe("single Impulse", () => {
       expect(result.current).not.toBe(initial)
       expect(count_1).toHaveProperty("emitters.size", 0)
       expect(count_2).toHaveProperty("emitters.size", 0)
-    })
+    },
+  )
 
-    it.concurrent("keeps attached Impulse", () => {
+  it.concurrent("keeps attached Impulse when re-renders", () => {
+    const count = Impulse.of(1)
+    const { result, rerender } = setup(count)
+
+    result.current()
+
+    rerender(count)
+
+    expect(count).toHaveProperty("emitters.size", 1)
+  })
+})
+
+describe("conditional Impulse", () => {
+  const setup = (impulse: Impulse<number>) => {
+    return renderHook(
+      (count: Impulse<number>) => {
+        return useScopedCallback(
+          (scope, isActive: boolean) => {
+            if (isActive) {
+              return count.getValue(scope) * 2
+            }
+
+            return -1
+          },
+          [count],
+        )
+      },
+      {
+        initialProps: impulse,
+      },
+    )
+  }
+
+  it.concurrent(
+    "the resultign function returns a fallback when not active",
+    () => {
       const count = Impulse.of(1)
-      const { result, rerender } = setup(count)
+      const { result } = setup(count)
 
-      result.current()
+      expect(result.current(false)).toBe(-1)
 
-      rerender(count)
+      act(() => {
+        count.setValue(2)
+      })
 
-      expect(count).toHaveProperty("emitters.size", 1)
+      expect(result.current(false)).toBe(-1)
+    },
+  )
+
+  it.concurrent("the resulting function returns a value when active", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    expect(result.current(true)).toBe(2)
+
+    act(() => {
+      count.setValue(2)
     })
+
+    expect(result.current(true)).toBe(4)
+  })
+
+  it.concurrent("does not attach an Impulse when not active", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    result.current(false)
+
+    expect(count).toHaveProperty("emitters.size", 0)
+  })
+
+  it.concurrent("attaches an Impulse when active", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    result.current(true)
+
+    expect(count).toHaveProperty("emitters.size", 1)
+  })
+
+  it.concurrent("keeps an Impulse attached ones it was active", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    result.current(true)
+    result.current(false)
+
+    expect(count).toHaveProperty("emitters.size", 1)
+  })
+
+  it.concurrent("de-attaches an Impulse when it was active", () => {
+    const count = Impulse.of(1)
+    const { result } = setup(count)
+
+    result.current(true)
+
+    act(() => {
+      count.setValue(2)
+    })
+
+    expect(count).toHaveProperty("emitters.size", 0)
   })
 })
