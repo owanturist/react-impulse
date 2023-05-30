2a48b36c8f14823cba5cf1d9a678e8cc695bf9da
Anton Ovechkin <owanturist@gmail.com>
2023-05-30 21:53:29 +0200

    Fix type of warnwarn

--- src/Impulse.ts
+++ src/Impulse.ts
@@ -12,18 +12,18 @@ export class Impulse<T> {
    *
    * @version 1.0.0
    */
-  // @warnwarn(
-  //   "subscribe",
-  //   "You should not call Impulse.of inside of the subscribe listener. The listener is for read-only operations but Impulse.of creates a new Impulse.",
-  // )
-  // @warnwarn(
-  //   "useScoped",
-  //   "You should not call Impulse.of inside of the useScoped factory. The useScoped hook is for read-only operations but Impulse.of creates a new Impulse.",
-  // )
-  // @warnwarn(
-  //   "useScopedMemo",
-  //   "You should not call Impulse.of inside of the useScopedMemo factory. The useScopedMemo hook is for read-only operations but Impulse.of creates a new Impulse.",
-  // )
+  @warnwarn(
+    "subscribe",
+    "You should not call Impulse.of inside of the subscribe listener. The listener is for read-only operations but Impulse.of creates a new Impulse.",
+  )
+  @warnwarn(
+    "useScoped",
+    "You should not call Impulse.of inside of the useScoped factory. The useScoped hook is for read-only operations but Impulse.of creates a new Impulse.",
+  )
+  @warnwarn(
+    "useScopedMemo",
+    "You should not call Impulse.of inside of the useScopedMemo factory. The useScopedMemo hook is for read-only operations but Impulse.of creates a new Impulse.",
+  )
   public static of<T>(
     initialValue: T,
     compare?: null | Compare<T>,
