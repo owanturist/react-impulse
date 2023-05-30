05f9fdc5ccb4019d0ee909d22dc1dc33a8198e3f
Anton Ovechkin <owanturist@gmail.com>
2023-05-30 21:27:19 +0200

    Declare types for esbuild-plugin-replace-regex

---
+++ types/globals.d.ts
@@ -0,0 +1,10 @@
+import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
+
+declare global {
+  namespace jest {
+    interface Matchers<R = void>
+      extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
+  }
+
+  const production: boolean
+}
