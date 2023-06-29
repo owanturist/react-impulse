04f3978313f1e3af63d8835fad317a1aa1d5d09a
Anton Ovechkin <owanturist@gmail.com>
2023-06-29 21:48:57 +0200

    Extend useImpulse and Impulse.of

--- src/useImpulse.ts
+++ src/useImpulse.ts
@@ -2,25 +2,38 @@ import { Impulse } from "./Impulse"
 import { Compare, isFunction, usePermanent, useEvent, eq } from "./utils"
 
 /**
+ * A hook that initiates a stable (never changing) Impulse without an initial value.
+ *
+ * @version 1.2.0
+ */
+export function useImpulse<T = undefined>(): Impulse<undefined | T>
+
+/**
  * A hook that initiates a stable (never changing) Impulse.
  *
  * *The initial value is disregarded during subsequent re-renders.*
  *
- * @param valueOrLazyValue either an initial value or function returning an initial value during the initial render
+ * @param valueOrInitValue either an initial value or function returning an initial value during the initial render
  * @param compare an optional `Compare` function applied as `Impulse#compare`. When not defined or `null` then `Object.is` applies as a fallback.
  *
  * @version 1.0.0
  */
 export function useImpulse<T>(
-  valueOrLazyValue: T | ((...args: []) => T),
+  valueOrInitValue: T | ((...args: []) => T),
   compare?: null | Compare<T>,
-): Impulse<T> {
+): Impulse<T>
+
+// Implements 👆
+export function useImpulse<T>(
+  valueOrInitValue?: T | ((...args: []) => T),
+  compare?: null | Compare<undefined | T>,
+): Impulse<undefined | T> {
   const stableCompare = useEvent(compare ?? eq)
 
   return usePermanent(() => {
-    const initialValue = isFunction(valueOrLazyValue)
-      ? valueOrLazyValue()
-      : valueOrLazyValue
+    const initialValue = isFunction(valueOrInitValue)
+      ? valueOrInitValue()
+      : valueOrInitValue
 
     return Impulse.of(initialValue, stableCompare)
   })
