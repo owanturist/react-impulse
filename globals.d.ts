f103bb4ba1fc0232cb6a76a57234e8e457f23c2b
Anton Ovechkin <owanturist@gmail.com>
2023-07-04 22:25:44 +0200

    Define scope in TestContext

--- globals.d.ts
+++ globals.d.ts
@@ -1,19 +1,14 @@
 import type { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers"
+import type { TestAPI } from "vitest"
 import type { Scope } from "./src"
 
-declare global {
-  namespace jest {
-    type Matchers<R = void, T = {}> = TestingLibraryMatchers<
-      typeof expect.stringContaining,
-      R
-    >
-  }
-}
+declare module "vitest" {
+  export interface JestAssertion<R = any>
+    extends TestingLibraryMatchers<typeof expect.stringContaining, R> {}
 
-declare module "@vitest/runner" {
   export interface TestContext {
     scope: Scope
   }
+
+  declare const it: TestAPI<TestContext>
 }
-
-
