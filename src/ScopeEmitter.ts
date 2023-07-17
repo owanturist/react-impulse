7a75fe7a1b92bc1747350f30c10104b786c61e18
Anton Ovechkin <owanturist@gmail.com>
2023-07-17 22:57:41 +0200

    Add valid change

--- src/ScopeEmitter.ts
+++ src/ScopeEmitter.ts
@@ -18,12 +18,12 @@ export class ScopeEmitter {
 
       ScopeEmitter.queue.push(execute())
 
-      const uniq = new WeakSet<VoidFunction>()
+      const uniqs = new WeakSet<VoidFunction>()
 
       ScopeEmitter.queue.forEach((emitters) => {
         emitters?.forEach((emitter) => {
-          if (!uniq.has(emitter.emit)) {
-            uniq.add(emitter.emit)
+          if (!uniqs.has(emitter.emit)) {
+            uniqs.add(emitter.emit)
             emitter.increment()
 
             if (emitter.shouldDetachOnEmit) {
