5fc2de71988452ddaad8c3b0fc209fb66685b529
Anton Ovechkin <owanturist@gmail.com>
2023-07-18 21:15:58 +0200

    Introduce tsup

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entryPoints: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  esbuildOptions(options) {
+    options.mangleProps = /^__/
+  },
+})
