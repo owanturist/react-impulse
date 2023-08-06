d9921623709b65f59490f7cae3b66afe4ce1b507
Anton Ovechkin <owanturist@gmail.com>
2023-08-06 09:03:02 +0200

    Don't define context for useImpulseCallback

--- src/useImpulseCallback.ts
+++ src/useImpulseCallback.ts
@@ -2,7 +2,6 @@ export { useImpulseCallback }
 
 import { type DependencyList, useMemo } from "./dependencies"
 import { useScope } from "./useScope"
-import { defineExecutionContext } from "./validation"
 import { injectScope } from "./Scope"
 
 /**
@@ -22,20 +21,11 @@ function useImpulseCallback<TArgs extends ReadonlyArray<unknown>, TResult>(
 ): (...args: TArgs) => TResult {
   const getScope = useScope()
 
-  // TODO useScopedMemo when scope is explicitly passed
   return useMemo(
     () => {
       const scope = getScope()
 
-      return (...args) => {
-        return defineExecutionContext(
-          "useImpulseMemo",
-          injectScope,
-          scope,
-          callback,
-          ...args,
-        )
-      }
+      return (...args) => injectScope(scope, callback, ...args)
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [...dependencies, getScope],
