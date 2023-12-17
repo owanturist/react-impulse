61f083184485df0cc7e014da0deabcedcc41d966
Anton Ovechkin <owanturist@gmail.com>
2023-12-17 14:25:01 +0100

    Delete singleThread

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globals: true,
-    singleThread: true,
+    // https://github.com/vitest-dev/vitest/issues/3967#issuecomment-1680585071
+    testTransformMode: {
+      ssr: ["**/*"],
+    },
     environment: "happy-dom",
     setupFiles: "./setup-tests.ts",
     coverage: {
