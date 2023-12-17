03b272fdba5245c87dc72bc75e80354fada1b815
Anton Ovechkin <owanturist@gmail.com>
2023-12-17 21:04:34 +0100

    Update deps (#582)
    * Update vitest
    
    * Delete singleThread
    
    * Update deps
    
    * Make useImpulseMemo dependencies reqired
    
    * Define coverage.exclude instead of full file c8-ignore

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     globals: true,
-    singleThread: true,
+    // https://github.com/vitest-dev/vitest/issues/3967#issuecomment-1680585071
+    testTransformMode: {
+      ssr: ["**/*"],
+    },
     environment: "happy-dom",
     setupFiles: "./setup-tests.ts",
     coverage: {
+      exclude: ["**/*.js", "**/*.d.ts", "setup-tests.ts", "src/messages.ts"],
       reporter: ["text", "json"],
     },
   },
