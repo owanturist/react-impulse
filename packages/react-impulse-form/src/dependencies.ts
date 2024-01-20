99d4a781bfaa3172618164b17bca9806a02d6c6d
Anton Ovechkin <owanturist@gmail.com>
2024-01-20 20:58:26 +0100

    Convert remeda to dev-deps

--- packages/react-impulse-form/src/dependencies.ts
+++ packages/react-impulse-form/src/dependencies.ts
@@ -17,4 +17,6 @@ export {
   untrack,
   useScoped,
 } from "react-impulse"
+// remeda is built-in to the bundle
+// eslint-disable-next-line import/no-extraneous-dependencies
 export { isFunction, isBoolean, isDefined, isTruthy, identity } from "remeda"
