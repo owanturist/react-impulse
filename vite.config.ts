3ae478a29158beb7efb63a7faa73bdd3f1c01621
Anton Ovechkin <owanturist@gmail.com>
2024-01-21 19:57:54 +0100

    Enable coverage autoUpdate

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from "vitest/config"
 export default defineConfig({
   test: {
     coverage: {
+      thresholds: {
+        autoUpdate: true,
+        "packages/react-impulse/src/**.ts": {
+          lines: 100,
+          statements: 100,
+          branches: 100,
+          functions: 100,
+        },
+      },
       include: ["**/src/**"],
       reporter: ["text", "json"],
     },
