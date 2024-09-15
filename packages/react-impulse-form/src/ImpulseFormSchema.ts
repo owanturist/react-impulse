3fbc109a3e8296ad894a064e95c3462b531288ef
Anton Ovechkin <owanturist@gmail.com>
2024-09-15 21:51:23 +0200

    Convert Result to a tuple

--- packages/react-impulse-form/src/ImpulseFormSchema.ts
+++ packages/react-impulse-form/src/ImpulseFormSchema.ts
@@ -3,7 +3,3 @@ import type { Schema, ZodPipeline, ZodTypeDef } from "zod"
 export type ImpulseFormSchema<TOutput, TInput = TOutput> =
   | Schema<TOutput, ZodTypeDef, TInput>
   | ZodPipeline<Schema<TInput>, Schema<TOutput>>
-
-export type Result<TError, TData> =
-  | { success: false; error: TError }
-  | { success: true; data: TData }
