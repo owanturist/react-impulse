7409917ec39170fa58e1ea79dbe69f0b09521f1b
Anton Ovechkin <owanturist@gmail.com>
2025-03-16 14:37:07 +0100

    Try to meet react19 API

--- packages/react-impulse/src/scoped.ts
+++ packages/react-impulse/src/scoped.ts
@@ -12,8 +12,8 @@ import {
   type ReactNode,
 } from "./dependencies"
 import type { Scope } from "./Scope"
-import type { Func } from "./utils"
 import { useScope } from "./useScope"
+import type { Func } from "./utils"
 
 export type PropsWithScope<TProps = Record<string, unknown>> = TProps & {
   scope: Scope
@@ -26,15 +26,66 @@ export type PropsWithoutScope<TProps> = "scope" extends keyof TProps
 export type ForwardedPropsWithoutScope<TRef, TProps> = RefAttributes<TRef> &
   PropsWithoutRef<PropsWithoutScope<TProps>>
 
-const renderWithScope = <TProps, TContext>(
+function renderWithScope<
+  TProps,
+  TContext,
+  TResult extends ReactNode | Promise<ReactNode>,
+>(
   scope: Scope,
-  component: Func<[PropsWithScope<TProps>, TContext], ReactNode>,
+  component: Func<[PropsWithScope<TProps>, unknown?], TResult>,
   props: TProps,
   ctx: TContext,
-): ReactNode => {
+): TResult {
   // it uses Object.assign to reduce output file size by avoiding the spread operator
   return component(Object.assign({}, props, { scope }), ctx)
 }
+
+function useInjectScope<TProps>(props: TProps): PropsWithScope<TProps> {
+  const getScope = useScope()
+
+  return Object.assign({}, props, { scope: getScope() })
+}
+
+function memo<TProps>(
+  component: FC<PropsWithScope<TProps>>,
+  propsAreEqual?: (
+    prev: Readonly<PropsWithoutScope<TProps>>,
+    next: Readonly<PropsWithoutScope<TProps>>,
+  ) => boolean,
+): MemoExoticComponent<FC<PropsWithoutScope<TProps>>> {
+  return React_memo(scoped(component), propsAreEqual)
+}
+
+function forwardRef<TRef, TProps>(
+  render: ForwardRefRenderFunction<TRef, PropsWithScope<TProps>>,
+): ForwardRefExoticComponent<ForwardedPropsWithoutScope<TRef, TProps>> {
+  return React_forwardRef<TRef, PropsWithoutScope<TProps>>((props, ref) => {
+    const propsWithScope = useInjectScope(props)
+
+    return render(propsWithScope as PropsWithScope<TProps>, ref)
+  })
+}
+
+function forwardRefMemo<TRef, TProps>(
+  render: ForwardRefRenderFunction<TRef, PropsWithScope<TProps>>,
+  propsAreEqual?: (
+    prev: Readonly<ForwardedPropsWithoutScope<TRef, TProps>>,
+    next: Readonly<ForwardedPropsWithoutScope<TRef, TProps>>,
+  ) => boolean,
+): MemoExoticComponent<
+  NamedExoticComponent<ForwardedPropsWithoutScope<TRef, TProps>>
+> {
+  return React_memo(forwardRef(render), propsAreEqual) as MemoExoticComponent<
+    NamedExoticComponent<ForwardedPropsWithoutScope<TRef, TProps>>
+  >
+}
+
+/**
+ * An alias for `React.memo(React.forwardRef(scoped(...)))`
+ *
+ * @version 1.0.0
+ */
+memo.forwardRef = forwardRef.memo = forwardRefMemo
 
 /**
  * Creates a React component that provides the `scope: Scope` property and subscribes to all Impulses calling the `Impulse#getValue` method during the rendering phase of the component.
@@ -48,7 +99,10 @@ export function scoped<TProps>(
   component: FC<PropsWithScope<TProps>>,
 ): FC<PropsWithoutScope<TProps>>
 export function scoped<TProps>(component: FC<TProps>): FC<TProps> {
-  const ComponentWithScope: FC<TProps> = (props, ctx: unknown) => {
+  const ComponentWithScope = (
+    props: TProps,
+    ctx?: unknown,
+  ): ReactNode | Promise<ReactNode> => {
     const getScope = useScope()
 
     return renderWithScope(getScope(), component, props, ctx)
@@ -60,45 +114,6 @@ export function scoped<TProps>(component: FC<TProps>): FC<TProps> {
 
   return ComponentWithScope
 }
-
-const memo = <TProps>(
-  component: FC<PropsWithScope<TProps>>,
-  propsAreEqual?: (
-    prev: Readonly<PropsWithoutScope<TProps>>,
-    next: Readonly<PropsWithoutScope<TProps>>,
-  ) => boolean,
-): MemoExoticComponent<FC<PropsWithoutScope<TProps>>> => {
-  return React_memo(scoped(component), propsAreEqual)
-}
-
-const forwardRef = <TRef, TProps>(
-  render: ForwardRefRenderFunction<TRef, PropsWithScope<TProps>>,
-): ForwardRefExoticComponent<ForwardedPropsWithoutScope<TRef, TProps>> => {
-  return React_forwardRef<TRef, PropsWithoutScope<TProps>>((props, ref) => {
-    const getScope = useScope()
-
-    return renderWithScope(getScope(), render, props as TProps, ref)
-  })
-}
-
-const forwardRefMemo = <TRef, TProps>(
-  render: ForwardRefRenderFunction<TRef, PropsWithScope<TProps>>,
-  propsAreEqual?: (
-    prev: Readonly<ForwardedPropsWithoutScope<TRef, TProps>>,
-    next: Readonly<ForwardedPropsWithoutScope<TRef, TProps>>,
-  ) => boolean,
-): MemoExoticComponent<
-  NamedExoticComponent<ForwardedPropsWithoutScope<TRef, TProps>>
-> => {
-  return React_memo(forwardRef(render), propsAreEqual)
-}
-
-/**
- * An alias for `React.memo(React.forwardRef(scoped(...)))`
- *
- * @version 1.0.0
- */
-memo.forwardRef = forwardRef.memo = forwardRefMemo
 
 /**
  * An alias for `React.memo(scoped(...))`
