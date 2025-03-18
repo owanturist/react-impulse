4a217801bee208f8b2d482a75bf4214d01bf65bb
Anton Ovechkin <owanturist@gmail.com>
2025-03-18 08:18:13 +0100

    Specify package level build and typecheck scripts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,77 +1,22 @@
-import { minify } from "terser"
-import { type Options, defineConfig } from "tsup"
+import { defineConfig } from "tsup"
 
-// Inspired by https://github.com/egoist/tsup/blob/692c112ac83f463d0be200253466c163c7cee36c/src/plugins/terser.ts
-/**
- * The plugin mangles only properties starting from single '_' and not ending at '_' without minification and mangling anything else.
- * The annotation looks like
- */
-const manglePlugin: Required<Options>["plugins"][0] = {
-  name: "terser-mangle",
-  renderChunk: async function (code, info) {
-    if (!/\.(c|m)?js$/.test(info.path)) {
-      return
-    }
-
-    try {
-      const minifiedOutput = await minify(code, {
-        compress: false,
-        output: {
-          beautify: true,
-          preserve_annotations: true,
-        },
-        mangle: {
-          nth_identifier: {
-            // by returning null it prevents undesired names mangling (ex: function arguments)
-            get: () => null as never,
-          },
-          module: false,
-          keep_classnames: true,
-          keep_fnames: true,
-          properties: {
-            regex: /^_[^_]\w+[^_]$/,
-            // @ts-expect-error undocumented, but it specifies how to mangle Impulse's properties
-            cache: {
-              props: {
-                $_value: "_",
-                $_emitters: "$",
-              },
-            },
-          },
-        },
-      })
-
-      this.logger.info("TERSER", "Mangling with Terser")
-      if (!minifiedOutput.code) {
-        this.logger.error("TERSER", "Failed to mangle with terser")
-      }
-      this.logger.success("TERSER", "Terser Mangling success")
-
-      return { code: minifiedOutput.code!, map: minifiedOutput.map }
-    } catch (error) {
-      this.logger.error("TERSER", "Failed to mangle with terser")
-      this.logger.error("TERSER", error)
-
-      return { code, map: info.map }
-    }
+export default defineConfig({
+  entry: ["./src/index.ts"],
+  outDir: "./dist",
+  format: ["cjs", "esm"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  terserOptions: {
+    mangle: {
+      module: false,
+      keep_classnames: true,
+      keep_fnames: true,
+      properties: {
+        regex: /^_[^_]\w+[^_]$/,
+      },
+    },
   },
-}
-
-export default defineConfig(({ env = {} }) => {
-  const packages = env.PACKAGE
-    ? [env.PACKAGE]
-    : ["react-impulse", "react-impulse-form"]
-
-  return packages.map((name) => ({
-    name,
-    entry: [`packages/${name}/src/index.ts`],
-    outDir: `packages/${name}/dist`,
-    format: ["cjs", "esm"],
-    dts: true,
-    sourcemap: true,
-    clean: true,
-    plugins: [manglePlugin],
-    // any package is external
-    external: [/^\w+/],
-  }))
+  // any package is external
+  external: [/^\w+/],
 })
