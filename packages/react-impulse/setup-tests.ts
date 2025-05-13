ce503ec6d221c35abaae2f025a40b2df8832598f
Anton Ovechkin <owanturist@gmail.com>
2025-05-13 09:26:03 +0200

    Do not pass variable in dynamic import

--- packages/react-impulse/setup-tests.ts
+++ packages/react-impulse/setup-tests.ts
@@ -4,8 +4,14 @@ import { untrack } from "./src"
 
 const spy_Object$is = vi.spyOn(Object, "is")
 
-if (process.env.TEST_TARGET != null) {
-  vi.mock("./src", () => import(`./${process.env.TEST_TARGET}`))
+if (process.env.TEST_TARGET === "dist/index.js") {
+  vi.mock("./src", () => import("./dist/index.js"))
+} else if (process.env.TEST_TARGET === "dist/index.cjs") {
+  vi.mock("./src", () => import("./dist/index.cjs"))
+} else if (process.env.TEST_TARGET != null) {
+  throw new Error(
+    `Invalid TEST_TARGET: ${process.env.TEST_TARGET}. Must be "dist/index.js" or "dist/index.cjs".`,
+  )
 }
 
 beforeEach((context) => {
