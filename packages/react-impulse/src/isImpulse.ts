73698c279549b2974f70b9acc9d7668d18037494
Anton Ovechkin <owanturist@gmail.com>
2025-05-14 15:44:48 +0200

    Introduce isImpulseFactory

--- packages/react-impulse/src/isImpulse.ts
+++ packages/react-impulse/src/isImpulse.ts
@@ -1,62 +1,46 @@
 import { BaseImpulse } from "./BaseImpulse"
 import type { Impulse, ReadonlyImpulse } from "./Impulse"
+import { isImpulseFactory } from "./isImpulseFactory"
 import type { Scope } from "./Scope"
-import type { Func } from "./utils"
 
-/**
- * A function to check whether or not the input is an Impulse.
- *
- * @version 3.0.0
- */
-export function isImpulse<T, Unknown = unknown>(
-  input: Unknown | Impulse<T>,
-): input is Impulse<T>
+export const isImpulse = isImpulseFactory(
+  (input) => input instanceof BaseImpulse,
+) as {
+  /**
+   * A function to check whether or not the input is an Impulse.
+   *
+   * @version 3.0.0
+   */
+  <T, Unknown = unknown>(input: Unknown | Impulse<T>): input is Impulse<T>
 
-/**
- * A function to check whether or not the input is an Impulse.
- *
- * @version 3.0.0
- */
-export function isImpulse<T, Unknown = unknown>(
-  input: Unknown | ReadonlyImpulse<T>,
-): input is ReadonlyImpulse<T>
+  /**
+   * A function to check whether or not the input is an Impulse.
+   *
+   * @version 3.0.0
+   */
+  <T, Unknown = unknown>(
+    input: Unknown | ReadonlyImpulse<T>,
+  ): input is ReadonlyImpulse<T>
 
-/**
- * A function to check whether or not an Impulse value passes the `check`.
- *
- * @version 3.0.0
- */
-export function isImpulse<T, Unknown = unknown>(
-  scope: Scope,
-  check: (value: unknown) => value is T,
-  input: Unknown | Impulse<T>,
-): input is Impulse<T>
+  /**
+   * A function to check whether or not an Impulse value passes the `check`.
+   *
+   * @version 3.0.0
+   */
+  <T, Unknown = unknown>(
+    scope: Scope,
+    check: (value: unknown) => value is T,
+    input: Unknown | Impulse<T>,
+  ): input is Impulse<T>
 
-/**
- * A function to check whether or not an Impulse value passes the `check`.
- *
- * @version 3.0.0
- */
-export function isImpulse<T, Unknown = unknown>(
-  scope: Scope,
-  check: (value: unknown) => value is T,
-  input: Unknown | ReadonlyImpulse<T>,
-): input is ReadonlyImpulse<T>
-
-export function isImpulse(
-  ...args:
-    | [input: unknown]
-    | [scope: Scope, check: Func<[unknown], boolean>, input: unknown]
-): boolean {
-  if (args.length === 1) {
-    return args[0] instanceof BaseImpulse
-  }
-
-  if (isImpulse(args[2])) {
-    const value = args[2].getValue(args[0])
-
-    return args[1](value)
-  }
-
-  return false
+  /**
+   * A function to check whether or not an Impulse value passes the `check`.
+   *
+   * @version 3.0.0
+   */
+  <T, Unknown = unknown>(
+    scope: Scope,
+    check: (value: unknown) => value is T,
+    input: Unknown | ReadonlyImpulse<T>,
+  ): input is ReadonlyImpulse<T>
 }
