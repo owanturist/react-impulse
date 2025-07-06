8e040ac846c94374a024310dc050041fb97ab6e4
Anton Ovechkin <owanturist@gmail.com>
2025-07-06 21:14:46 +0200

    Unskip tests

--- packages/react-impulse-form/tests/impulse-form-unit/is-validated.spec.ts
+++ packages/react-impulse-form/tests/impulse-form-unit/is-validated.spec.ts
@@ -266,7 +266,7 @@ describe.each([
       expect(value.getError(scope)).toBeNull()
     })
 
-    it.skip("marks as validated on submit", async ({ scope }) => {
+    it("marks as validated on submit", async ({ scope }) => {
       const value = setup({ validateOn })
 
       await value.submit()
