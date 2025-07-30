054fc459aef3e5e4918ff244a0971432d192fc03
Anton Ovechkin <owanturist@gmail.com>
2025-07-30 11:00:49 +0200

    Convert tools to a private package

--- packages/tools/vitest.config.ts
+++ packages/tools/vitest.config.ts
@@ -1,10 +1,7 @@
-import tsconfigPaths from "vite-tsconfig-paths"
 import { defineProject } from "vitest/config"
 import type { ProjectConfig } from "vitest/node"
 
 export default defineProject({
-  plugins: [tsconfigPaths()],
-
   test: {
     globals: true,
     poolOptions: {
