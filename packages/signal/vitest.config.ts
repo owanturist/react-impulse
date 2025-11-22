fee7a856570daa114781fb12e4aca3de3f66538b
Anton Ovechkin <owanturist@gmail.com>
2025-11-22 22:48:10 +0100

    Rename react-impulse -> signal

--- packages/signal/vitest.config.ts
+++ packages/signal/vitest.config.ts
@@ -3,7 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths"
 import { defineProject } from "vitest/config"
 import type { ProjectConfig } from "vitest/node"
 
-console.log(`Running react-impulse tests against react@${React.version}`)
+console.log(`Running @owanturist/signal tests against react@${React.version}`)
 
 export default defineProject({
   plugins: [tsconfigPaths()],
