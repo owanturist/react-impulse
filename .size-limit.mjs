e010487b729e7c710f3df2c6acef836db54dd32d
Anton Ovechkin <owanturist@gmail.com>
2026-01-24 22:11:42 +0100

    Update size limit files

--- .size-limit.mjs
+++ .size-limit.mjs
@@ -1,17 +1,22 @@
 export default [
   {
     name: "signal",
-    ignore: ["use-sync-external-store"],
+    files: ["index", "monitor-factory"],
   },
   {
     name: "signal-form",
     ignore: ["@owanturist/signal"],
   },
+  {
+    name: "signal-react",
+    ignore: ["@owanturist/signal", "react", "use-sync-external-store"],
+  },
 ]
   .flatMap((config) => [".js", ".cjs"].map((ext) => ({ ...config, ext })))
-  .map(({ name, ignore, ext }) => ({
-    name: `${name}${ext}`,
-    path: `packages/${name}/dist/index${ext}`,
+  .flatMap(({ files = ["index"], ...config }) => files.map((file) => ({ ...config, file })))
+  .map(({ name, file, ignore, ext }) => ({
+    name: `${name}/${file}${ext}`,
+    path: `packages/${name}/dist/${file}${ext}`,
     gzip: true,
-    ignore: ["react", ...ignore],
+    ignore,
   }))
